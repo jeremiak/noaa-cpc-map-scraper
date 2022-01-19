862d59e11246739ba6afc6e0100436567355e185
Jeremia Kimelman <jbkimelman@gmail.com>
2022-01-18 22:03:02 -0800

    Try Linux compatible date?

--- scripts/soil-moisture-anomaly-daily.sh
+++ scripts/soil-moisture-anomaly-daily.sh
@@ -1,3 +1,3 @@
-YESTERDAY=$(date -v-1d +"%Y-%m-%d")
+YESTERDAY=$(date --date=yesterday +"%Y-%m-%d")
 OUTPUT=$(echo "images/$YESTERDAY-soil-moisture-anomaly-daily.gif")
 curl https://www.cpc.ncep.noaa.gov/products/Soilmst_Monitoring/Figures/daily/curr.w.anom.daily.gif -o $OUTPUT
--- scripts/soil-moisture-mm-daily.sh
+++ scripts/soil-moisture-mm-daily.sh
@@ -1,3 +1,3 @@
-YESTERDAY=$(date -v-1d +"%Y-%m-%d")
+YESTERDAY=$(date --date=yesterday +"%Y-%m-%d")
 OUTPUT=$(echo "images/$YESTERDAY-soil-moisture-mm-daily.gif")
 curl https://www.cpc.ncep.noaa.gov/products/Soilmst_Monitoring/Figures/daily/curr.w.full.daily.gif -o $OUTPUT
